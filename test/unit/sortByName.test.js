d58d9816c71023f673cd53bd1507f4b4e49c9575
unknown <evshkryabiy@google.com>
2022-04-01 14:22:40 +0300

    Code 100% covered

---
+++ test/unit/sortByName.test.js
@@ -0,0 +1,43 @@
+const sorting = require("../../app");
+
+describe("Books names test suit", () => {
+  it("Books names should be sorted in ascending order", () => {
+    expect(
+      sorting.sortByName([
+        "Гарри Поттер",
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер",
+    ]);
+  });
+  it("Books names should not be sorted", () => {
+    expect(
+      sorting.sortByName([        
+        "Властелин Колец",
+        "Волшебник изумрудного города",
+        "Гарри Поттер"
+      ])
+    ).toEqual([
+      "Властелин Колец",
+      "Волшебник изумрудного города",
+      "Гарри Поттер"
+    ]);
+  });
+  it("Books names should be sorted when ", () => {
+    expect(
+      sorting.sortByName([  
+        "Волшебник изумрудного города",      
+        "Волшебник изумрудного города",         
+        "Гарри Поттер"
+      ])
+    ).toEqual([
+      "Волшебник изумрудного города", 
+      "Волшебник изумрудного города",
+      "Гарри Поттер"
+    ]);
+  });
+});
